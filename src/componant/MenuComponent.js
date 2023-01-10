ce6d8c41d8b4c7c1f3f3cf13a57fd9d447462897
developython14 <djmel1955@gmail.com>
2023-01-10 07:52:29 +0100

    work on lifecycle widegts and pros and states

--- src/componant/MenuComponent.js
+++ src/componant/MenuComponent.js
@@ -46,16 +46,13 @@ constructor(props){
 render(){
     const menu = this.state.dishes.map((dish) => {
         return (
-          <div key={dish.id} className="col-12 mt-5">
-            <Media tag="li">
-              <Media left middle>
-                  <Media object src={dish.image} alt={dish.name} />
-              </Media>
-              <Media body className="ml-5">
-                <Media heading>{dish.name}</Media>
-                <p>{dish.description}</p>
-              </Media>
-            </Media>
+          <div key={dish.id} className="col-12 mt-5 d-flex flex-row align-items-center">
+            <img src={dish.image} alt={dish.name} />
+            <div className='d-flex flex-column ms-3'>
+            <h3>{dish.name}</h3>
+            <p>{dish.description}</p>
+            </div>
+          
           </div>
         );
     });
