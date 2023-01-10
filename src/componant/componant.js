2a0877d3ff931ad868f652c82a7e2c99e25c988c
developython14 <djmel1955@gmail.com>
2023-01-10 05:37:04 +0100

    work on lifecycle widegts and pros and states

--- src/componant/componant.js
+++ src/componant/componant.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-
+import { Media  , Button} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Welcome extends Component {
     constructor(props) {
         super(props);
-        this.state = {'name':'mustapha'}
+        this.state = {'name':'mustapha' , 'color' :'green'}
+        this.handleClick = this.handleClick.bind(this);
+        this.handlecolor = this.changecolor.bind(this);
       }
+      
 
-
+      handleClick() {
+        this.setState(prevState => ({
+          'name': Math.random()
+        }));
+      }
+      changecolor() {
+        this.setState(prevState => ({
+          'color': 'red'
+        }));
+      }
+    
     render() {
       return <div>
-        <h3>this.state.name</h3>
+        <h3 style={{color:this.state.color}}>{this.state.name}</h3>
+        <Button onClick={this.handleClick}>update</Button>
+        <Button onClick={this.handlecolor}>change color</Button>
       </div>
     }
   }
