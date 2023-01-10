74baf3e2ada457ba3ed78f90cc4367a7c853b356
developython14 <djmel1955@gmail.com>
2023-01-10 09:44:47 +0100

    work on lifecycle

--- src/componant/DishdetailComponent.js
+++ src/componant/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Media  , Button ,Card ,CardBody, CardImg ,CardImgOverlay,CardText ,CardTitle } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class DishdetailComponent  extends Component {
     constructor(props){
@@ -7,9 +8,13 @@ class DishdetailComponent  extends Component {
     }
 
     render(){
+        const comments = this.props.dish.comments.map((comment) => {
+            return (<p>comment.comment</p>);
+        });
         return (
-            <div>
-                salam alikom
+            <div className='d-flex flex-column col-12 col-md-5'>
+                <h3>Comments</h3>
+                {comments}
             </div>
         )
     }
