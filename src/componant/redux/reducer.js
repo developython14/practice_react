8b0df77a65c22941b8d347d92889d5f755ae31bd
developython14 <djmel1955@gmail.com>
2023-01-15 06:12:43 +0100

    intgrate redux into the react app

--- src/componant/redux/reducer.js
+++ src/componant/redux/reducer.js
@@ -3,3 +3,14 @@ import { LEADERS } from "../leaders";
 import { PROMOTIONS } from "../PROMOTIONS";
 import { COMMENTS } from "../COMMENTS";
 
+
+export const initialState = {
+    dishes:DISHES,
+    leaders:LEADERS,
+    promotions:PROMOTIONS,
+    Comments:COMMENTS
+}
+
+export const Reducer = (state,action)=>{
+return state
+};
